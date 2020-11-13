b499e33f2a0c2cc315f4d978bbad7a6bd6a58b60
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-11-13 16:11:57 -0600

    Refactored code

---
+++ src/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const grid = document.querySelector(".grid");
+    let squares = Arrary.from(document.querySelectorAll(".grid div"));
+    const width = 10;
+})
