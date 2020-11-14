cdc42f94568dfbc4d5ee4a476f9e53a05f16ba39
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-11-13 18:58:59 -0600

    Refactored code

--- src/app.js
+++ src/app.js
@@ -4,5 +4,67 @@ document.addEventListener("DOMContentLoaded", () => {
     const score = document.querySelector("#score");
     const startBtn = document.querySelector("#start-btn");
     const width = 10;
-    console.log(squares);
+
+    // Tetris Blocks
+    const LBlock = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const ZBlock = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const TBlock = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const OBlock = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const IBlock = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const allTetrisBlocks = [LBlock, ZBlock, TBlock, OBlock, IBlock]
+
+
+    // console.log(squares);
 })
+
+
+
+
+
+
+
+
+
+
+// // Tetris Blocks
+// const LBlock = [
+//     ["one"],
+//     ["two"],
+//     ["three"],
+//     ["four"]
+// ]
+
+
+// // Array.prototype.forEach()
+// LBlock.forEach(x=>console.log(x));
+
