712ae5efe29307abaed06f67a41047b545d93f1b
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-11-13 21:27:33 -0600

    Refactored code

--- src/app.js
+++ src/app.js
@@ -11,40 +11,55 @@ document.addEventListener("DOMContentLoaded", () => {
         [width, width + 1, width + 2, width * 2 + 2],
         [1, width + 1, width * 2 + 1, width * 2],
         [width, width * 2, width * 2 + 1, width * 2 + 2]
-    ]
+    ];
 
     const ZBlock = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
     ]
 
     const TBlock = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
     ]
 
     const OBlock = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
-    ]
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ];
 
     const IBlock = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
-    ]
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ];
 
-    const allTetrisBlocks = [LBlock, ZBlock, TBlock, OBlock, IBlock]
+    const allTetrisBlocks = [LBlock, ZBlock, TBlock, OBlock, IBlock];
 
+    let currentPosition = 4;
+    // let currentBlock = allTetrisBlocks[0][0];
+    // let currentBlock = allTetrisBlocks[0][0];
+    // let currentBlock = allTetrisBlocks[0][0];
+    // let currentBlock = allTetrisBlocks[1][0];
 
-    // console.log(squares);
+    // random tetris block
+    let randomBlock = Math.floor(Math.random() * allTetrisBlocks.length);
+    let currentBlock = allTetrisBlocks[randomBlock][0];
+
+    function draw() {
+        currentBlock.forEach(index => {
+            squares[currentPosition + index].classList.add("tetris-block");
+        })
+    }
+
+    draw();
 })
 
 
