be13504cd05c37ea114acee7c3c0aad46a1a5d81
DBerrones <53327546+danielberrones@users.noreply.github.com>
2020-11-20 17:33:51 -0600

    Updated thru CMD tool

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tetrisGrid = document.querySelector("#tetris-grid");
+    let tetrisCellsArray = Array.from(document.querySelectorAll("tetris-cells"));
+    const width = 10;
+
+    let timerID;
 
 
+    
+    
 
+    
+})
 
 
 
